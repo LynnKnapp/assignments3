1a8f5beb1c0b7cf5a2efb07b7f705bac6dce6028
Lynn Knapp <ebonyeek@example.com>
2019-09-06 12:41:43 -0600

    pokemon

--- array-sort/main.js
+++ array-sort/main.js
@@ -1,10 +1,36 @@
-let arr = [1, 3, 5, 2, 90, 20]
+// let arr = [1, 3, 5, 2, 90, 20]
 
-let  leastToGreatest = 
-     arr.sort(function (a,b) {
-        return a - b
-     }) 
+// function leastToGreatest(arr){ 
+//     return arr.sort(function(a,b) {
+//         return a - b
+//     }) 
      
+// } 
+//   console.log(leastToGreatest([1, 3, 5, 2, 90, 20]))
+
+//   function greatestToLeast(arr) {
+//     return arr.sort(function (a, b){
+//         return b - a
+//     })
+//   }
   
+//   console.log(greatestToLeast([1, 3, 5, 2, 90, 20]))
+
+  function lengthSort(arr) {
+   return arr.sort(function (a, b){
+          return a.length - b.length
+      })
+  }
   
-  console.log(leastToGreatest)
+  console.log(lengthSort(["dog", "wolf", "by", "family", "eaten"])) 
+
+
+function alphabetical(arr) {
+    return arr.sort(a,b){
+        
+    }
+    }
+    
+    console.log(alphabetical(["dog", "wolf", "by", "family", "eaten"]))
+
+    
