55bc75c04ab902fd35c26a348433d26ba00f847a
Lynn Knapp <ebonyeek@gmail.com>
2020-02-17 14:25:01 -0500

    changed pawesome pets link

--- react/projects/portfolio/portfolio/src/components/Projects.js
+++ react/projects/portfolio/portfolio/src/components/Projects.js
@@ -8,8 +8,9 @@ const Projects =(props)=>{
         <div className= 'projects-container'ref={props.projectsRef}>
             <h1>Projects</h1>
             <div className='img-container'>
-                <a href='http://serenitynow.surge.sh/'><img classname ='img'src = {seinfeldImg} alt='seinfeld' /></a>
-                <a href='https://pawesompets.herokuapp.com/'><img className ='img'src = {pawesome} alt='pawesome' /></a>
+                <a href='http://serenitynow.surge.sh/'>Seinfeld API Quiz</a>
+                <a href='https://pawesome-pets1.herokuapp.com/'>Pet Adoption Fullstack App</a>
+                {/* <a href='https://recipe-app14.herokuapp.com/'>Recipe App</a> */}
             </div>    
         </div>
     )
